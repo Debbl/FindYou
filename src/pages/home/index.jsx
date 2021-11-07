c304060464b33bf6a6d4ab587c1f20f95c51d7d7
符嘉辉 <2303058491@qq.com>
2021-11-07 11:05:49 +0800

    remove styled.js

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import { HomeStyled } from './home.styled';
+
 import { getItemsListData } from '../../common/items.date';
 import MenuCate from '../../components/menu-cate';
 import AppItemCate from '../../components/app-item-cate';
 import useFormatData from '../../hooks/use-format-data';
-
+import styled from 'styled-components';
 function Home() {
   const ItemsListData = useFormatData();
 
   return (
     <HomeStyled>
-      <div  className="show-menu-cate">
+      <div className="show-menu-cate">
         <MenuCate />
       </div>
       <div className="show-cate">
@@ -27,5 +27,19 @@ function Home() {
     </HomeStyled>
   )
 }
+const HomeStyled = styled.div`
+  .show-menu-cate {
+    position: fixed;
+    top: 0;
+    height: 100vh;
+    width: 16vw;
+    border-right: 2px solid black;
+  }
+  
+  .show-cate {
+    margin-left: 20vw;
+  }
+`
+
 
 export default Home;
