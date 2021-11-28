78fa5227765ce8d303dc78626215cd3da80406f4
Debbl <1685294601@qq.com>
2021-11-28 12:13:10 +0800

    :lipstick: (icon.png) Add icon
    添加图标

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,15 +1,24 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "My Gatsby Site",
+    siteUrl: 'https://www.yourdomain.tld',
+    title: 'My Gatsby Site'
   },
   plugins: [
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `markdown-pages`,
-        path: `${__dirname}/content`,
-      },
+        path: `${__dirname}/content`
+      }
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Find You`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        icon: `./src/images/icon.png`
+      }
     },
     `gatsby-transformer-remark`,
     `gatsby-plugin-styled-components`
